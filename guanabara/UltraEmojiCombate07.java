9fd10828d78f348ebb7b2a0a19b15439462407d5
Laura Lobo <lauralobo364@gmail.com>
2023-11-13 15:50:12 -0300

    atualizando commits

---
+++ guanabara/UltraEmojiCombate07.java
@@ -0,0 +1,10 @@
+package guanabara;
+
+public class UltraEmojiCombate07 {
+    public static void main (String[] args) {
+        Lutador l = new Lutador("Pretty Boy", "França", 31, 1.75f,
+                                68.9f, 11, 2, 1);
+        
+        l.status();
+    }
+}
